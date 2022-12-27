778487164d0a8f04a2d384b17c109aa370d5bdf2
Sackarias Lunman <lunman@student.chalmers.se>
2022-12-27 15:52:43 +0100

    added a little motor animation. todo: fix load torque on motor

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ size = width, height = 1200, 600
 screen = pygame.display.set_mode(size)
 
 dt = 0.0005
-runtime = 10
-N = int(runtime / dt)
 g = 9.81
 
 system = DCMotor(12, -12, 0.05, 0, 0.5, 0.5, 0.05, 0.01, (0,0,0), 
@@ -28,6 +26,7 @@ def si_to_pixels(x: float):
 
 last_update = 0
 start_time = time()
+i = 0
 
 while True:
     current_time = time()
@@ -39,37 +38,37 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    for i in range(1,N):
-        system.update(dt, 5*sin(0.001*i), g)
+    system.update(dt, 5*sin(0.001*i) + 2*cos(0.005*i), g)
 
-        screen.fill((255,255,255))
+    screen.fill((255,255,255))
 
-        x0 = si_to_pixels(system.cart.x()) + width//2
-        y0 = height//2
-        pygame.draw.rect(screen, system.cart.color, (x0, y0, 20, 10))
+    x0 = si_to_pixels(system.cart.x()) + width//2
+    y0 = height//2
+    pygame.draw.rect(screen, system.cart.color, (x0, y0, 20, 10))
 
-        max_x = width//2 + si_to_pixels(system.cart.max_x)
-        min_x = width//2 + si_to_pixels(system.cart.min_x)
-        pygame.draw.rect(screen, system.cart.color, (min_x-10, y0, 10, 10))
-        pygame.draw.rect(screen, system.cart.color, (max_x+20, y0, 10, 10))
+    max_x = width//2 + si_to_pixels(system.cart.max_x)
+    min_x = width//2 + si_to_pixels(system.cart.min_x)
+    pygame.draw.rect(screen, system.cart.color, (min_x-10, y0, 10, 10))
+    pygame.draw.rect(screen, system.cart.color, (max_x+20, y0, 10, 10))
 
-        motor_x0 = min_x-100
-        motor_x1 = si_to_pixels(sin(system.angle())*0.05)
-        motor_y1 = si_to_pixels(cos(system.angle())*0.05)
+    motor_x0 = min_x-100
+    motor_x1 = si_to_pixels(sin(-system.angle())*0.05)
+    motor_y1 = si_to_pixels(cos(-system.angle())*0.05)
 
-        pygame.draw.polygon(screen, system.color, [
-            (motor_x0+motor_x1, y0+motor_y1),
-            (motor_x0+motor_x1, y0-motor_y1),
-            (motor_x0-motor_x1, y0-motor_y1),
-            (motor_x0-motor_x1, y0+motor_y1),
-        ])
+    pygame.draw.polygon(screen, system.color, [
+        (motor_x0+motor_x1, y0+motor_y1),
+        (motor_x0+motor_y1, y0-motor_x1),
+        (motor_x0-motor_x1, y0-motor_y1),
+        (motor_x0-motor_y1, y0+motor_x1),
+    ])
 
-        x0 += 10
-        for pole in iter(system.cart):
-            x1 = x0 + si_to_pixels(pole.l * sin(pole.angle()))
-            y1 = y0 + si_to_pixels(-pole.l * cos(pole.angle()))
-            pygame.draw.line(screen, pole.color, (x0, y0), (x1, y1), 10)
-            x0 = x1
-            y0 = y1
-        
-        pygame.display.flip()
+    x0 += 10
+    for pole in iter(system.cart):
+        x1 = x0 + si_to_pixels(pole.l * sin(pole.angle()))
+        y1 = y0 + si_to_pixels(-pole.l * cos(pole.angle()))
+        pygame.draw.line(screen, pole.color, (x0, y0), (x1, y1), 10)
+        x0 = x1
+        y0 = y1
+    
+    pygame.display.flip()
+    i += 1
