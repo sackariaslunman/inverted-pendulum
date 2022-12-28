e455349a7c08c2114918ba128a404fba97134d1c
Sackarias Lunman <lunman@student.chalmers.se>
2022-12-28 17:57:09 +0100

    implemented torque load, the systems seems to be working correctly :) (except convservation of momentum but thats fine)

--- cartpole.py
+++ cartpole.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from math import sin, cos, pi
 
 class DCMotor:
-    def __init__(self, max_V: float, min_V: float, resistance: float, inductance: float, ke: float, kt: float, inertia: float, friction: float, color: tuple[int,int,int], cart: Cart):
+    def __init__(self, max_V: float, min_V: float, resistance: float, inductance: float, ke: float, kt: float, inertia: float, friction: float, radius: float, color: tuple[int,int,int]):
         self.max_V = max_V
         self.min_V = min_V
 
@@ -13,47 +13,40 @@ class DCMotor:
         self.Jm = inertia
         self.Bm = friction
 
-        self.cart = cart
         self.color = color
+        self.r = radius
 
         self.state = {
-            "theta": [0.0],
-            "Wm": [0.0],
-            "Ia": [0.0]
+            "omega": [0],
+            "d_omega": [0]
         }
 
     def angle(self, t=None):
         if not t:
-            t = len(self.state["theta"])-1
-        return self.state["theta"][t]
+            t = len(self.state["omega"])-1
+        return self.state["omega"][t]
 
     def angular_velocity(self, t=None):
         if not t:
-            t = len(self.state["Wm"])-1
-        return self.state["Wm"][t]
+            t = len(self.state["d_omega"])-1
+        return self.state["d_omega"][t]
 
-    def update(self, dt: float, Va: float, g: float):
-        Wm = self.angular_velocity()
-        
-        Ia = (Va-self.Kt*Wm)/self.Ra
-        Tm = self.Kt*Ia
-        d_Wm = (Tm-self.Bm*Wm-0)/self.Jm
-        Wm = Wm + d_Wm * dt
-        theta = self.angle() + Wm * dt
+    def update(self, dt, d_x):
+        d_omega = d_x / self.r
+        omega = self.angle() + d_omega * dt
 
-        self.state["theta"].append(theta)
-        self.state["Wm"].append(Wm)
-        self.state["Ia"].append(Ia)
-
-        self.cart.update(dt, Tm, g)
+        self.state["d_omega"].append(d_omega)
+        self.state["omega"].append(omega)
 
 class Cart:
-    def __init__(self, mass: float, cart_friction: float, x0: float, min_x: float, max_x: float, color: tuple[int,int,int], pole: Pole):
+    def __init__(self, mass: float, cart_friction: float, x0: float, min_x: float, max_x: float, color: tuple[int,int,int], motor: DCMotor, pole: Pole):
         self.m = mass
         self.u_c = cart_friction
         self.min_x = min_x
         self.max_x = max_x
         self.color = color
+
+        self.motor = motor
         
         self.state = {
             "x": [x0],
@@ -68,7 +61,7 @@ class Cart:
 
     def total_mass(self):
         M = self.m
-        for pole in iter(self.pole):
+        for pole in self:
             M += pole.m
         return M
 
@@ -82,6 +75,11 @@ class Cart:
             t = len(self.state["d_x"])-1
         return self.state["d_x"][t]
 
+    def acceleration(self, t=None):
+        if not t:
+            t = len(self.state["dd_x"])-1
+        return self.state["dd_x"][t]
+
     def clamp(self, x: float):
         if x > self.max_x:
             return self.max_x
@@ -89,17 +87,17 @@ class Cart:
             return self.min_x
         return x
 
-    def update(self, dt: float, u: float, g: float):
+    def update(self, dt: float, Va: float, g: float):
         x = self.x()
         d_x = self.velocity()
         M = self.total_mass()
 
-        sum1 = g*sum([pole.m*sin(pole.angle())*cos(pole.angle()) for pole in iter(self)])
-        sum2 = -7/3*(u+sum([pole.m*pole.lh()*pole.angular_velocity()**2*sin(pole.angle()) for pole in iter(self)])-self.u_c*d_x)
-        sum3 = -sum([pole.u_p*pole.angular_velocity()*cos(pole.angle())/pole.lh() for pole in iter(self)])
-        sum4 = sum([pole.m*cos(pole.angle())**2 for pole in iter(self)])-7/3*M
+        sum1 = sum([pole.m*sin(pole.angle())*cos(pole.angle()) for pole in self])
+        sum2 = sum([pole.m*pole.lh()*pole.angular_velocity()**2*sin(pole.angle()) for pole in self])
+        sum3 = sum([pole.u_p*pole.angular_velocity()*cos(pole.angle())/pole.lh() for pole in self])
+        sum4 = sum([pole.m*cos(pole.angle())**2 for pole in self])
 
-        dd_x = (sum1+sum2+sum3)/sum4
+        dd_x = (g*sum1-7/3*((1/self.motor.r**2)*(self.motor.Kt/self.motor.Ra*(Va*self.motor.r-self.motor.Ke*d_x)-self.motor.Bm*d_x)+sum2-self.u_c*d_x)-sum3)/(sum4-7/3*(M+self.motor.Jm/self.motor.r**2))
         d_x = d_x + dd_x * dt
         x = self.clamp(x + d_x * dt)
 
@@ -107,8 +105,9 @@ class Cart:
         self.state["d_x"].append(d_x)
         self.state["x"].append(x)
 
-        for pole in iter(self):
+        for pole in self:
             pole.update(dt, dd_x, g)
+        self.motor.update(dt, d_x)
 
 class Pole:
     def __init__(self, mass: float, angle0: float, length: float, pole_friction: float, color: tuple[int,int,int], child: Pole | None):
@@ -120,7 +119,6 @@ class Pole:
         self.state = {
             "theta": [angle0],
             "d_theta": [0],
-            "dd_theta": [0]
         }
 
         self.child = child
@@ -154,7 +152,6 @@ class Pole:
         d_theta = d_theta + dd_theta * dt
         theta = theta + d_theta * dt
 
-        self.state["dd_theta"].append(dd_theta)
         self.state["d_theta"].append(d_theta)
         self.state["theta"].append(theta)
 
--- main.py
+++ main.py
@@ -8,14 +8,26 @@ size = width, height = 1200, 600
 
 screen = pygame.display.set_mode(size)
 
-dt = 0.0005
+class Colors:
+    red = (255,0,0)
+    green = (0,255,0)
+    blue = (0,0,255)
+    purple = (255,0,255)
+    cyan = (0,255,255)
+    yellow = (255,255,0)
+    black = (0,0,0)
+    white = (255,255,255)
+    gray = (100,100,100)
+colors = Colors
+
+dt = 0.001
 g = 9.81
 
-system = DCMotor(12, -12, 0.05, 0, 0.5, 0.5, 0.05, 0.01, (0,0,0), 
-    Cart(0.1, 0.05, 0, -0.8, 0.8, (255,0,0), 
-        Pole(0.1, 10/180*pi, 0.2, 0.01, (0,255,0), 
-            Pole(0.1, 5/180*pi, 0.2, 0.01, (0,0,255), 
-                Pole(0.1, 15/180*pi, 0.2, 0.01, (255,0,255), None)
+cart = Cart(0.5, 0.05, 0, -0.8, 0.8, colors.red,
+    DCMotor(12, -12, 0.05, 0, 0.5, 0.5, 0.05, 0.01, 0.05, colors.black),
+    Pole(0.2, -10/180*pi, 0.2, 0.005, colors.green, 
+        Pole(0.2, -5/180*pi, 0.15, 0.005, colors.blue, 
+            Pole(0.2, -15/180*pi, 0.15, 0.005, colors.purple, None
             )
         )
     )
@@ -28,47 +40,68 @@ last_update = 0
 start_time = time()
 i = 0
 
+font = pygame.font.Font('freesansbold.ttf', 20)
+
 while True:
-    current_time = time()
+    current_time = time()-start_time
     if current_time > dt + last_update:
         last_update = current_time
     else:
         continue
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: 
+            sys.exit()
 
-    system.update(dt, 0, g)
+    screen.fill(colors.gray)
 
-    screen.fill((255,255,255))
+    cart.update(dt, 30*cos(dt*i*10), g)
 
-    x0 = si_to_pixels(system.cart.x()) + width//2
+    x0 = si_to_pixels(cart.x()) + width//2
     y0 = height//2
-    pygame.draw.rect(screen, system.cart.color, (x0, y0, 20, 10))
+    pygame.draw.rect(screen, cart.color, (x0, y0, 20, 10))
 
-    max_x = width//2 + si_to_pixels(system.cart.max_x)
-    min_x = width//2 + si_to_pixels(system.cart.min_x)
-    pygame.draw.rect(screen, system.cart.color, (min_x-10, y0, 10, 10))
-    pygame.draw.rect(screen, system.cart.color, (max_x+20, y0, 10, 10))
+    max_x = width//2 + si_to_pixels(cart.max_x)
+    min_x = width//2 + si_to_pixels(cart.min_x)
+    pygame.draw.rect(screen, cart.color, (min_x-10, y0, 10, 10))
+    pygame.draw.rect(screen, cart.color, (max_x+20, y0, 10, 10))
 
     motor_x0 = min_x-100
-    motor_x1 = si_to_pixels(sin(-system.angle())*0.05)
-    motor_y1 = si_to_pixels(cos(-system.angle())*0.05)
+    motor_sin = si_to_pixels(sin(-cart.motor.angle())*0.05)
+    motor_cos = si_to_pixels(cos(-cart.motor.angle())*0.05)
 
-    pygame.draw.polygon(screen, system.color, [
-        (motor_x0+motor_x1, y0+motor_y1),
-        (motor_x0+motor_y1, y0-motor_x1),
-        (motor_x0-motor_x1, y0-motor_y1),
-        (motor_x0-motor_y1, y0+motor_x1),
+    pygame.draw.polygon(screen, cart.motor.color, [
+        (motor_x0+motor_sin, y0+motor_cos),
+        (motor_x0+motor_cos, y0-motor_sin),
+        (motor_x0-motor_sin, y0-motor_cos),
+        (motor_x0-motor_cos, y0+motor_sin),
     ])
 
     x0 += 10
-    for pole in iter(system.cart):
+    for pole in cart:
         x1 = x0 + si_to_pixels(pole.l * sin(pole.angle()))
         y1 = y0 + si_to_pixels(-pole.l * cos(pole.angle()))
         pygame.draw.line(screen, pole.color, (x0, y0), (x1, y1), 10)
         x0 = x1
         y0 = y1
     
+    texts = [
+        f"Time: {round(i*dt,2)} s",
+        "",
+        "Cart:",
+        f"Position: {round(cart.x(),2)} m",
+        f"Velocity: {round(cart.velocity(),2)} m/s",
+        f"Acceleration: {round(cart.acceleration(),2)} m/s^2",
+        "",
+        "Motor:",
+        f"Angle: {round(cart.motor.angle(),2)} rad",
+        f"Angular velocity: {round(cart.motor.angular_velocity(),2)} rad/s",
+    ]
+    
+    for k, text_k in enumerate(texts):
+        text = font.render(text_k, True, colors.black, colors.gray)
+        text_rect = text.get_rect()
+        screen.blit(text,(0,(text_rect.height)*k,text_rect.width,text_rect.height))
+
     pygame.display.flip()
     i += 1
