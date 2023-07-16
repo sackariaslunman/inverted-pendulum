cae3d304eb3d8c3d3bb51731f48e1f1276282525
Sackarias Lunman <lunman@student.chalmers.se>
2023-07-16 16:33:39 +0200

    stuff

--- src/embedded/serial-communication/src/main.cpp
+++ src/embedded/serial-communication/src/main.cpp
@@ -6,10 +6,10 @@ long last_update = 0;
 int dt = 10;
 
 float state[] = {1,2,3,4,5,6};
-float control[14400];
+float control[15600];
 
 void setup() {
-  Serial.begin(576000);
+  Serial.begin(500000);
   Serial.setTimeout(10*1000);
   Serial.write("start");
   pinMode(LED,OUTPUT);
@@ -28,7 +28,8 @@ void loop() {
   Serial.write((uint8_t*)state, sizeof(state));
 
   if (Serial.available()) {
-    if (Serial.read() == 's') {
+
+    if (Serial.read() == 't') {
       digitalWrite(LED, HIGH);
       Serial.readBytes((uint8_t*)control, sizeof(control));
       delay(50);
