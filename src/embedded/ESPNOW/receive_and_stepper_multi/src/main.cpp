18b24443e93ea83f5db3570fe20c6498df804176
Johan Wheeler <84925732+johanwheeler@users.noreply.github.com>
2023-08-16 19:48:18 +0200

    Cleaned, added define for one or two pendulums

--- src/embedded/ESPNOW/receive_and_stepper_multi/src/main.cpp
+++ src/embedded/ESPNOW/receive_and_stepper_multi/src/main.cpp
@@ -16,6 +16,11 @@
 
 #include "encoder.h"
 
+// Define number of pendulums
+#define ONE_PENDULUM
+// #define TWO_PENDULUMS
+
+
 // Time
 unsigned long startMillis = 0;
 
@@ -30,54 +35,32 @@ typedef struct struct_message {
 struct_message recivedData;
 struct_message pendulumData_0;
 struct_message pendulumData_1;
-// struct_message 2Data;
-// struct_message 3Data;
 struct_message cartData;
-
 
 // Encoder objects
 RotaryEncoder pendulum_0;
 RotaryEncoder pendulum_1;
 RotaryEncoder cart;
 
-// array for counting espnow updates per id
-int update_count[5] = {0,0,0,0,0};
-
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&recivedData, incomingData, sizeof(recivedData));
-  // Serial.print("Bytes received: ");
-  // Serial.print(len);
-  // Serial.print("  Encoder_pos: ");
-  // Serial.print(recivedData.encoder_pos);
-  // Serial.print("  Sender ID: ");
-  // Serial.print(recivedData.sender_id);  
-  // Serial.println();
 
   // Update struct with corresponding id
   if (recivedData.sender_id == 0){
     // pendulumData_0 = recivedData;
     pendulum_0.update(recivedData.encoder_pos);
-    update_count[0] += 1;
   }
   else if (recivedData.sender_id == 1){
     // pendulumData_1 = recivedData;
     pendulum_1.update(recivedData.encoder_pos);
-    update_count[1] += 1;
-  }
-  else if (recivedData.sender_id == 2){
-    // 2Data = recivedData;
-  }
-  else if (recivedData.sender_id == 3){
-    // 3Data = recivedData;
   }
   else if (recivedData.sender_id == 4){
     // cartData = recivedData;
     cart.update(recivedData.encoder_pos);
-    update_count[4] += 1;
   }
   else{
-    Serial.println("Error: Invalid sender ID");
+    // Serial.println("Error: Invalid sender ID");
   }
 }
 
@@ -114,12 +97,15 @@ long last_loop = 0;
 int dt = 10000;
 
 // State  = pos (m), vel (m/s), angle (rad), angle_vel (rad/sec)
-double state[] = {0,0,2,0,0,0};
+#ifdef ONE_PENDULUM
+double state[] = {0,0,0,0};
+#endif
+#ifdef TWO_PENDULUMS
+double state[] = {0,0,0,0,0,0};
+#endif
 // Control = Linear acceleration (m/s^2) of the cart
 double control[] = {0};
-
 double control_stepperSpeed  = 0; // m/s
-
 
 
 void setup() {
@@ -133,12 +119,9 @@ void setup() {
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    // Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
 
   // Setup stepper
@@ -167,15 +150,12 @@ void loop() {
 
     state[0] = stepper.currentPosition() * length_per_step;
     state[1] = stepper.speed() * length_per_step;
-    // state[2] = pendulum_0.getPos();
-    // state[3] = pendulum_0.getVel();
-    // state[4] = pendulum_1.getPos() + pendulum_0.getPos(); 
-    // state[5] = pendulum_1.getVel() + pendulum_0.getVel();
-
     state[2] = pendulum_0_pos;
     state[3] = pendulum_0_vel;
+    #ifdef TWO_PENDULUMS
     state[4] = pendulum_1_pos + pendulum_0_pos + PI; 
     state[5] = pendulum_1_vel + pendulum_0_vel;
+    #endif
 
     Serial.write((uint8_t*)state, sizeof(state));
 
@@ -199,11 +179,11 @@ void loop() {
     // Limit speed
     if (control_stepperSpeed > maxSpeed)
     {
-      control_stepperSpeed = maxSpeed-1;
+      control_stepperSpeed = maxSpeed;
     }
     else if (control_stepperSpeed < -maxSpeed)
     {
-      control_stepperSpeed = -maxSpeed+1;
+      control_stepperSpeed = -maxSpeed;
     }
     stepper.setSpeed(control_stepperSpeed / length_per_step);
 
@@ -211,46 +191,5 @@ void loop() {
   }
 
   stepper.runSpeed();
-
-
-
-// See if 100 ms has passed
-  // if (millis() - startMillis > 1000)  {
-  //   // Reset the timer
-  //   startMillis = millis();
-
-  //   // Print encoder pos for each id
-  //   Serial.print("Pendulum 0: ");
-  //   Serial.print(pendulum_0.getPos());
-  //   Serial.print("  Pendulum 1: ");
-  //   Serial.print(pendulum_1.getPos());
-  //   Serial.print("  Cart: ");
-  //   Serial.print(cart.getPos());
-
-  //   // Velocity for each id
-  //   Serial.print("  Pendulum 0: ");
-  //   Serial.print(pendulum_0.getVel());
-  //   Serial.print("  Pendulum 1: ");
-  //   Serial.print(pendulum_1.getVel());
-  //   Serial.print("  Cart: ");
-  //   Serial.print(cart.getVel());
-
-
-  //   // Print update count for each id
-  //   // Serial.print("  Pendulum 0: ");
-  //   // Serial.print(update_count[0]);
-  //   // Serial.print("  Pendulum 1: ");
-  //   // Serial.print(update_count[1]);
-  //   // Serial.print("  Cart: ");
-  //   // Serial.print(update_count[4]);
-
-  //   // clear update count
-  //   update_count[0] = 0;
-  //   update_count[1] = 0;
-  //   update_count[4] = 0;
-
-
-  //   Serial.println();
-  // }
 
 }
